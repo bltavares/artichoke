110836b2e349646d8c7af7674742d0bb66c4bf53
Bruno Tavares <bruno.exz@gmail.com>
2015-08-03 21:01:59 -0300

    Initial idea for the extraction interface

---
+++ tests/parsing.rs
@@ -0,0 +1,20 @@
+extern crate artichoke;
+
+#[test]
+fn extracts_h_articles() {
+    let example = "
+    <html>
+        <head>
+          <title>A Title</title>
+        </head>
+        <body>
+          <article class='h-entry'>
+              This should be your article.
+          </article>
+        </body>
+    </html>
+    ";
+
+   let result = artichoke::parse(&example);
+   assert_eq!(result, Article { body = "This should be your article." });
+}
